094673a4496b311b6bde67878f2eedb0e345c099
kamalkiran undefined <kamalkiran1204@gmail.com>
2026-04-30 19:35:33 +0530

    Add files via upload

---
+++ data strucutute ex 22.cpp
@@ -0,0 +1,16 @@
+#include<Stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node*next;
+}*top;
+
+void push(int ele)
+{
+	struct node*n;
+	n=(struct node *)malloc(sizeof(struct node));
+	n->data=
+	
+}
